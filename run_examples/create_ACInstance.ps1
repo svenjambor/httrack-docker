dcfae6df8ab838b13b8fd465b3600c2843e3b14e
svenjambor <sven.jambor@gmail.com>
2021-01-13 10:20:13 +0100

    removed --dns-label
    prevents container from getting public IP

--- run_examples/create_ACInstance.ps1
+++ run_examples/create_ACInstance.ps1
@@ -1,27 +1,19 @@
 az login
 $SUBSCRIPTIONID = (az account show --query id -o tsv)
 
-#$WEBSITE="Full URL of site to copy, incl. http(s)"
+$WEBSITE="Full URL of site to copy, incl. http(s)"
 
-#$SA_RG= "Resource Group of storage account to write into"
-#$SA_NAME="Name of storage account to write into"
+$SA_RG= "Resource Group of storage account to write into"
+$SA_NAME="Name of storage account to write into"
 
-#$ACI_RG="Resource Group Name or Container Instance" 
-#$ACI_NAME="Name to use for container instance, all lowercase"
-#$ACI_ID_SCOPE="/subscriptions/$SUBSCRIPTIONID/resourceGroups/$SA_RG/providers/Microsoft.Storage/storageAccounts/$SA_NAME"
-
-$WEBSITE="https://dynamic.smurfje.com"
-$SA_RG= "RG_Webcontent_NEU"
-$SA_NAME="sastaticsmurfje"
-$ACI_RG="RG_Webcontent_NEU" 
-$ACI_NAME="acidlsmurfje"
-$ACI_ID_SCOPE="/subscriptions/$SUBSCRIPTIONID/resourceGroups/$SA_RG/providers/Microsoft.Storage/storageAccounts/$SA_NAME/blobServices/default"
+$ACI_RG="Resource Group Name or Container Instance" 
+$ACI_NAME="Name to use for container instance, all lowercase"
+$ACI_ID_SCOPE="/subscriptions/$SUBSCRIPTIONID/resourceGroups/$SA_RG/providers/Microsoft.Storage/storageAccounts/$SA_NAME"
 
 az container create `
     --resource-group $ACI_RG `
     --name $ACI_NAME `
     --image "sjambor/httrack:latest" `
-    --dns-name-label $ACI_NAME `
     --restart-policy Never `
     --assign-identity `
     --scope $ACI_ID_SCOPE `
