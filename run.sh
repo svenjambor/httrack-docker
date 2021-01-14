3a82e49733b9d5583a4ca15c2e68088db149d41f
svenjambor <sven.jambor@gmail.com>
2021-01-14 09:58:33 +0100

    REmoved AZCopy login
    azcopy login is implied when running azcopy copy in MSI mode; causes time out to use it.

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 /usr/bin/httrack $WEBSITE
 
 #check for AZCOPY_AUTO_LOGIN_TYPE=MSI to see if we're on Azure; build something else for Docker later
-/usr/bin/azcopy login
 /usr/bin/azcopy copy "${WEBSITE#*//}/*" "https://$AZ_BLOB_SA.blob.core.windows.net/$AZ_BLOB_CONTAINER" --recursive
